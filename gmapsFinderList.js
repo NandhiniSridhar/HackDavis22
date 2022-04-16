0240aa0806e8414b719f3877ed4f089c0fd0317f
Kaoushik Murugan <63953483+KMMineCube@users.noreply.github.com>
2022-04-16 15:04:42 -0700

    minor formatting changes

--- gmapsFinderList.js
+++ gmapsFinderList.js
@@ -8,8 +8,8 @@ const CONFIGURATION = {
     "mapOptions": {"center":{"lat":38.0,"lng":-100.0},"fullscreenControl":true,"mapTypeControl":false,"streetViewControl":false,"zoom":4,"zoomControl":true,"maxZoom":17},
     "mapsApiKey": "AIzaSyAGVZtNgRySaf0LB7lpT1MfAddZm55xAnA",
     "capabilities": {"input":true,"autocomplete":true,"directions":false,"distanceMatrix":true,"details":false}
-  };
+};
 
 function initMap() {
     new LocatorPlus(CONFIGURATION);
-  }
+}
