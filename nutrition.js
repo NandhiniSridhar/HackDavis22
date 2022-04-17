48a594f62264459207d042187f07412f9da2d3b3
s02singh <103849999+s02singh@users.noreply.github.com>
2022-04-16 22:40:56 -0700

    Update nutrition.js

--- nutrition.js
+++ nutrition.js
@@ -20,7 +20,8 @@ async function getInfo(){
     text = document.getElementById("text_input");
     console.log(text.value);
     url = 'https://api.edamam.com/api/nutrition-data?app_id=912b689e&app_key=%209398e2a012f26a313d77e69893220af0%09&nutrition-type=logging&ingr=' + text.value
-
+    
+    
     // getData(url).then((res) => {
     //     console.log(res);
     //     //console.log(Object.keys(res));
@@ -29,16 +30,32 @@ async function getInfo(){
 
     res = await getData(url);
     console.log(res);
+
+    document.getElementById("calories").innerHTML = "Calories:" + Number( res.calories.toPrecision(3) );
+    document.getElementById("fat").innerHTML = "Fat: " + Number( res.totalDaily.FAT.quantity.toPrecision(3) );
+    document.getElementById("carbs").innerHTML = "Carbs:" + Number( res.totalDaily.CHOCDF.quantity.toPrecision(3) );
     //console.log(Object.keys(res));
+    // document.getElementById("new_text").value = "";
 
-    let fat = document.createElement('p');
-    fat.innerHTML = "fat: " + res.totalDaily.FAT.quantity;
+    // let calories = document.createElement('p')
+    // let cal= Number( res.calories.toPrecision(3) );
+    // calories.innerHTML = "calories: " + cal;
 
-    let carbs = document.createElement('p')
-    carbs.innerHTML = "carbs: " + res.totalDaily.CHOCDF.quantity;
+    // let fat = document.createElement('p');
+    // let f= Number( res.totalDaily.FAT.quantity.toPrecision(3) );
+    // fat.innerHTML = "fat: " + f;
 
-    document.getElementById("new_text").appendChild(fat);
-    document.getElementById("new_text").appendChild(carbs);
+    // let carbs = document.createElement('p')
+    // let carb = Number( res.totalDaily.CHOCDF.quantity.toPrecision(3) );
+    // carbs.innerHTML = "carbs: " + carb;
+
+
+
+    // document.getElementById("new_text").appendChild(calories);
+    // document.getElementById("new_text").appendChild(fat);
+    // document.getElementById("new_text").appendChild(carbs);
+    
+
 
     
     
