0565d41bef4b0b35b1ce94c4fce8d74a689544e0
Nandhini <nandhini.sridhar02@gmail.com>
2022-04-16 20:25:28 -0700

    fixed getData - need to finish

--- nutrition.js
+++ nutrition.js
@@ -1,27 +1,45 @@
 //alert("hello");
 
 "use: strict"
+
+
 function getData(url){
-    fetch(url)
+    return fetch(url)
 	    .then(response => response.json())
-	    //.then(response => console.log(response))
-        .then(response => console.log(typeof response))
-        .then(reponse => console.log(Object.keys(reponse)))
-        .then(response => {return response})
+	    // .then(response => console.log(response))
+        // .then(response => console.log(typeof response))
+        // .then(reponse => console.log(Object.keys(reponse)))
+        //.then(response => {return response})
         //.then(console.log(response.properties.uri))
         //.then(response => parseRes(response))
 	    .catch(err => console.error(err));
 }
 
 
-function getInfo(){
+async function getInfo(){
     text = document.getElementById("text_input");
     console.log(text.value);
     url = 'https://api.edamam.com/api/nutrition-data?app_id=912b689e&app_key=%209398e2a012f26a313d77e69893220af0%09&nutrition-type=logging&ingr=' + text.value
 
-    res = getData(url);
-    //console.log(res);
+    // getData(url).then((res) => {
+    //     console.log(res);
+    //     //console.log(Object.keys(res));
+    //     document.getElementById("new_text").innerHTML = res.healthLabels;
+    // });
+
+    res = await getData(url);
+    console.log(res);
     //console.log(Object.keys(res));
-    document.getElementById("new_text").innerHTML = res;
+
+    let fat = document.createElement('p');
+    fat.innerHTML = "fat: " + res.totalDaily.FAT.quantity;
+
+    let carbs = document.createElement('p')
+    carbs.innerHTML = "carbs: " + res.totalDaily.CHOCDF.quantity;
+
+    document.getElementById("new_text").appendChild(fat);
+    document.getElementById("new_text").appendChild(carbs);
+
+    
     
 }
