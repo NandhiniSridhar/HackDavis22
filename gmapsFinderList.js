fdaa63946c72e5a55dfa690e1a3a9fb2d0acc3c1
Kaoushik Murugan <63953483+KMMineCube@users.noreply.github.com>
2022-04-16 17:39:11 -0700

    I need help :cry:

--- gmapsFinderList.js
+++ gmapsFinderList.js
@@ -1,10 +1,31 @@
-const CONFIGURATION = {
-    "locations": [
-      {"title":"Arbuckle Family Action Center - Food Distribution Center","address1":"812 King St","address2":"Arbuckle, CA 95912, USA","coords":{"lat":39.01439993842376,"lng":-122.06056887790986},"placeId":"ChIJAQAUhDWZhIAR0ZQWL21sRgE"},
-      {"title":"Campers Inn RV Park - Food Distribution Center","address1":"2501 County Road 88","address2":"Arbuckle, CA 95912, USA","coords":{"lat":38.903410580718216,"lng":-121.98950262331351},"placeId":"ChIJ4_-jMfS9hIARiSu4-tWGIB4"},
-      {"title":"Clarksburg Community Church","address1":"52910 Netherlands Ave","address2":"Clarksburg, CA 95612, USA","coords":{"lat":38.41672981341151,"lng":-121.52665900613421},"placeId":"ChIJJxrHy1HJmoARVby44lyD3pc"},
-      {"title":"Clarksburg Firehouse - Food Distribution Center","address1":"52902 Clarksburg Rd","address2":"Clarksburg, CA 95612, USA","coords":{"lat":38.41998792083203,"lng":-121.52966098834324},"placeId":"ChIJ-f-LoFPJmoARFidTAauwnsA"}
-    ],
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbOfuO5c8l2ji2UBBGlAoEr8wHdLL3XLg",
+  authDomain: "helping-hand-bbbaf.firebaseapp.com",
+  projectId: "helping-hand-bbbaf",
+  storageBucket: "helping-hand-bbbaf.appspot.com",
+  messagingSenderId: "905591168700",
+  appId: "1:905591168700:web:243f434809a4225ea0bab9",
+  measurementId: "G-FCCLBPW3ZQ"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getFoodBanks(db) {
+  const foodBanksCol = collection(db, 'Food Banks');
+  const foodBanksSnapshot = await getDocs(foodBanksCol);
+  const foodBanksList = foodBanksSnapshot.docs.map(doc => doc.data());
+  return foodBanksList;
+}
+
+CONFIGURATION = {
+  "locations": getFoodBanks(db),
+  
     "mapOptions": {"center":{"lat":38.0,"lng":-100.0},"fullscreenControl":true,"mapTypeControl":false,"streetViewControl":false,"zoom":4,"zoomControl":true,"maxZoom":17},
     "mapsApiKey": "AIzaSyAGVZtNgRySaf0LB7lpT1MfAddZm55xAnA",
     "capabilities": {"input":true,"autocomplete":true,"directions":false,"distanceMatrix":true,"details":false}
